49d0dcc8ae2c3d8e88ff075d4ccc4e4d9b8ad52d
oxmc <67136658+oxmc@users.noreply.github.com>
2022-10-09 17:20:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ const fs = require('fs')
 const path = require('path')
 
 const globals = require('./globals.js')
-const bundler = require('./bundler.js')
+const bundlercss = require('./css-bundler.js')
+const bundlerjs = require('./js-bundler.js')
 
-function bundle(options = {}) {
+const bunlde = {
+  "js": function (options = {}) {
 
     if (!options.entry) {
         throw new Error('options.entryfilepath was not defined')
     }
 
     let entryfilepath = path.normalize(path.resolve(process.cwd(), options.entry))
-    let bundled = bundler.bundle(entryfilepath, options)
+    let bundled = bundlerjs.bundle(entryfilepath, options)
 
     if (options.dest) {
         let dest = path.normalize(path.resolve(process.cwd(), options.dest || globals.DEFAULT_OUTPUT_FILE))
@@ -31,6 +33,35 @@ function bundle(options = {}) {
     }
 
     return bundled
+  },
+  "css": function (options = {}) {
+
+    if (!options.entry) {
+        throw new Error('options.entryfilepath was not defined')
+    }
+
+    let entryfilepath = path.normalize(path.resolve(process.cwd(), options.entry))
+    let bundled = bundlercss.bundle(entryfilepath, options)
+
+    if (options.dest) {
+        let dest = path.normalize(path.resolve(process.cwd(), options.dest || globals.DEFAULT_OUTPUT_FILE))
+        if (!fs.existsSync(path.dirname(dest))) {
+            fs.mkdirSync(path.dirname(dest))
+        }
+        fs.writeFileSync(dest, bundled, { encoding: options.encoding || 'utf-8' })
+    }
+    if (options.print) {
+        process.stdout.write(bundled)
+    } else {
+        console.log('Bundled:', bundled.split('\n'), 'lines')
+        if (options.dest) {
+            console.log('Wrote to file:', options.dest)
+        }
+        console.log('Done.')
+    }
+
+    return bundled
+  }
 }
 
 module.exports = bundle
